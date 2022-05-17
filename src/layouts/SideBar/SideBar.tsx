4c8fdedbb27213bd51ef74da7ba894e929299adc
dr1verrr <darfor2107@gmail.com>
2022-05-17 23:29:19 +0300

    feat(layouts): add main UI

---
+++ src/layouts/SideBar/SideBar.tsx
@@ -0,0 +1,3 @@
+export default function SideBar(): JSX.Element {
+  return <div>SideBar</div>
+}
---
+++ src/layouts/index.ts
@@ -0,0 +1,5 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import SideBar from './SideBar/SideBar'
+
+export { Header, Footer, SideBar }
