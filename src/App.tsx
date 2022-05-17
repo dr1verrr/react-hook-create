4f6c6b9ce8cee81cc51db0bb75fcf86f9260739a
dr1verrr <darfor2107@gmail.com>
2022-05-18 00:50:43 +0300

    feat(progress): add progress bar while component loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { lazy, Suspense } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import { Header } from './layouts'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Box } from '@mui/material'
 
 const SignIn = lazy(() => import('./views/SignIn'))
 const SignUp = lazy(() => import('./views/SignUp'))
@@ -13,7 +15,13 @@ function App(): JSX.Element {
       <Router>
         <Header />
 
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 5 }}>
+              <CircularProgress />
+            </Box>
+          }
+        >
           <Routes>
             <Route path='/signin' element={<SignIn />} />
             <Route path='/signup' element={<SignUp />} />
