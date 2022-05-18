a0dcbf631bfcd3e4fc562bcef9cba636c6b4163e
dr1verrr <darfor2107@gmail.com>
2022-05-18 19:18:54 +0300

    chore(firebase config): export firebase app

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -11,5 +11,5 @@ export const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
