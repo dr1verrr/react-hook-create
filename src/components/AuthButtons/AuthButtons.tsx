8a992a923c98ba4c7e9603783f2b6b039dc5bbe6
dr1verrr <darfor2107@gmail.com>
2022-05-18 19:14:58 +0300

    feat: add onClick handlers for authentication buttons

--- src/components/AuthButtons/AuthButtons.tsx
+++ src/components/AuthButtons/AuthButtons.tsx
@@ -2,18 +2,19 @@ import { Button, ButtonGroup } from '@mui/material'
 import GoogleIcon from '@mui/icons-material/Google'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import { signinWithGithubPopup, signinWithGooglePopup, signout } from '../../app/auth'
 
 export default function AuthButtons() {
   return (
     <ButtonGroup size='large'>
       <Button>
-        <GoogleIcon />
+        <GoogleIcon onClick={signinWithGooglePopup} />
       </Button>
       <Button>
-        <GitHubIcon />
+        <GitHubIcon onClick={signinWithGithubPopup} />
       </Button>
       <Button>
-        <PermIdentityIcon />
+        <PermIdentityIcon onClick={signout} />
       </Button>
     </ButtonGroup>
   )
