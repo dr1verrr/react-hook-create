f75504cebf585ece786287ecd4a34ee45dead0f2
dr1verrr <darfor2107@gmail.com>
2022-05-18 19:11:41 +0300

    chore: add auth action methods to export

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -1,5 +1,8 @@
 import signin from './auth.signin.password'
 import signup from './auth.signup.password'
 import signout from './auth.signout'
+import signinWithGithubPopup from './auth.github.signin.popup'
+import signinWithGooglePopup from './auth.google.signin.popup'
+import { getAuthData } from './auth.helpers'
 
-export { signin, signup, signout }
+export { signin, signup, signout, signinWithGooglePopup, signinWithGithubPopup, getAuthData }
