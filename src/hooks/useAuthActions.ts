4ea7982d4c4919017c77e0214f5083134da6fdf8
dr1verrr <darfor2107@gmail.com>
2022-05-18 19:17:52 +0300

    feat(useAuthActions): create hook returning auth methods

---
+++ src/hooks/useAuthActions.ts
@@ -0,0 +1,13 @@
+import { signin, signinWithGithubPopup, signinWithGooglePopup, signout, signup } from '../app/auth'
+
+const useAuthActions = () => {
+  return {
+    login: signin,
+    loginWithGithubPopup: signinWithGithubPopup,
+    loginWithGooglePopup: signinWithGooglePopup,
+    logout: signout,
+    register: signup,
+  }
+}
+
+export default useAuthActions
