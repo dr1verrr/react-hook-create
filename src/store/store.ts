f3af1d0cdbceb409a20551657f805c4a71372104
dr1verrr <darfor2107@gmail.com>
2022-05-20 01:59:11 +0300

    chore: move store to specified file

---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './rootReducer'
+
+const store = configureStore({ reducer: rootReducer })
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+export default store
