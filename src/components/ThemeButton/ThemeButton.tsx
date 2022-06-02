7e547aa822baa41ffcdb84d43b80a01b37b32ab6
dr1verrr <darfor2107@gmail.com>
2022-06-02 16:37:50 +0300

    feat(theme): create ThemeButton

---
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -0,0 +1,22 @@
+import { DarkModeOutlined, LightModeOutlined } from '@mui/icons-material'
+import { IconButton, IconButtonProps, useTheme } from '@mui/material'
+import { useDispatch } from 'react-redux'
+
+import { switchTheme } from 'store/ui/ui.actions'
+
+const ThemeButton = (props: IconButtonProps) => {
+  const dispatch = useDispatch()
+  const toggleMode = () => dispatch(switchTheme())
+  const {
+    palette: { mode }
+  } = useTheme()
+
+  return (
+    <IconButton {...props} onClick={props.onClick || toggleMode}>
+      {mode === 'light' && <LightModeOutlined sx={{ color: '#000' }} />}
+      {mode === 'dark' && <DarkModeOutlined />}
+    </IconButton>
+  )
+}
+
+export default ThemeButton
