1c7418ee749ad048fcbd7b4008125f4de3ea7b6b
dr1verrr <darfor2107@gmail.com>
2022-06-02 16:39:16 +0300

    feat: create AuthButtons(auth buttons group)

--- src/components/AuthButtons/AuthButtons.tsx
+++ src/components/AuthButtons/AuthButtons.tsx
@@ -1,21 +1,37 @@
-import { Button, ButtonGroup } from '@mui/material'
-import GoogleIcon from '@mui/icons-material/Google'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import GoogleIcon from '@mui/icons-material/Google'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import { Button, ButtonGroup } from '@mui/material'
+import { toast } from 'react-toastify'
+
 import { signinWithGithubPopup, signinWithGooglePopup } from 'app/auth'
-import { errorHandler } from 'utils'
 import { signAsGuest } from 'app/auth'
+import { errorHandler } from 'handlers'
 
 export default function AuthButtons() {
+  const onSuccess = (message: string) => toast(message, { icon: '🙌' })
+
   return (
     <ButtonGroup size='large'>
-      <Button onClick={() => errorHandler(signinWithGooglePopup)}>
+      <Button
+        onClick={() => {
+          errorHandler(signinWithGooglePopup, () => onSuccess('Signed in via Google provider.'))
+        }}
+      >
         <GoogleIcon />
       </Button>
-      <Button onClick={() => errorHandler(signinWithGithubPopup)}>
+      <Button
+        onClick={() => {
+          errorHandler(signinWithGithubPopup, () => onSuccess('Signed in via Github provider.'))
+        }}
+      >
         <GitHubIcon />
       </Button>
-      <Button onClick={() => errorHandler(signAsGuest)}>
+      <Button
+        onClick={() => {
+          errorHandler(signAsGuest, () => onSuccess('Signed in anonymously.'))
+        }}
+      >
         <PermIdentityIcon />
       </Button>
     </ButtonGroup>
