cdb05dd8c158855f557fcffd55cf443d8e3270b8
dr1verrr <darfor2107@gmail.com>
2022-06-02 16:06:08 +0300

    refactor: remove helper files

--- src/utils/helpers/removeObjectField.ts
+++
@@ -1,8 +0,0 @@
-const removeObjectField = (fieldsToRemove: object, object: any) => {
-  const modifiedObject = { ...object }
-  Object.keys(fieldsToRemove).forEach(fld => delete modifiedObject[fld])
-
-  return modifiedObject
-}
-
-export default removeObjectField
