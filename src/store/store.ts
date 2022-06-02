2c8b0cab7bba9d8aa335bb38e9a8e08b7dab19fb
dr1verrr <darfor2107@gmail.com>
2022-06-02 16:18:57 +0300

    refactor(store): export typed useAppSelector, useAppDispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
 import rootReducer from './rootReducer'
 
-const store = configureStore({ reducer: rootReducer })
+export const store = configureStore({
+  reducer: rootReducer
+})
 
 store.subscribe(() => {
   console.log(store.getState())
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export default store
