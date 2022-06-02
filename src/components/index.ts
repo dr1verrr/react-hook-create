76adb84a9e12ad7441aaf06d464578d24f3743bd
dr1verrr <darfor2107@gmail.com>
2022-06-02 16:39:59 +0300

    chore: export created components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,9 @@
 import AuthButtons from './AuthButtons/AuthButtons'
-import Authenticated from './Authenticated/Authenticated'
 import Copyright from './Copyright/Copyright'
+import Features from './Features/Features'
+import Hero from './Hero/Hero'
+import ThemeButton from './ThemeButton/ThemeButton'
+import { UserAvatar } from './User'
+import UserDetailedModal from './User/UserDetailedModal'
 
-export { Copyright, AuthButtons, Authenticated }
+export { Copyright, AuthButtons, ThemeButton, Hero, Features, UserAvatar, UserDetailedModal }
