4c1675ee7cad77af62378b86282fdf2c9623ca39
dr1verrr <darfor2107@gmail.com>
2022-06-02 15:45:19 +0300

    chore: add security rules(database)

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,15 +1,34 @@
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
 
-export const firebaseConfig = {
+//import { createFirestoreInstance } from 'redux-firestore'
+
+//import { store } from 'store'
+
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
-export const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+const firebaseApp = initializeApp(firebaseConfig)
+
+const auth = getAuth(firebaseApp)
+
+//const rrfConfig = {
+//  userProfile: 'users',
+//  useFirestoreForProfile: true
+//}
+
+//const rrfProps = {
+//  firebase: firebaseApp,
+//  config: rrfConfig,
+//  dispatch: store.dispatch,
+//  createFirestoreInstance
+//}
+
+export { firebaseConfig, firebaseApp, auth }
